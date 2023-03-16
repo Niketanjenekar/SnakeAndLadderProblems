f3b232bb9f6dc300f5a796456bee79603c9075cb
“Niketanjenekar” <niketanjenekar31@gmail.com>
2023-03-16 23:24:28 +0530

    Welcome to the Snake and Laddeer Game

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace SnakeAndLadderProblem
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Snake and Ladder Single Player Game");
+
+            SnakeandLadder_UC1.SnakeLadderProg_Usecase1();
+            SnakeandLadder_UC2.SnakeLadderProg_Usecaase2();
+        }
+    }
+}
