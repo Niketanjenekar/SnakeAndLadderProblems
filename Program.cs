2761c6e8eaf5eb8ebe879387116cd0981a4d955d
“Niketanjenekar” <niketanjenekar31@gmail.com>
2023-03-16 23:28:28 +0530

    Snake and ladder program with use case 1

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@
             Console.WriteLine("Welcome to Snake and Ladder Single Player Game");
 
             SnakeandLadder_UC1.SnakeLadderProg_Usecase1();
-            SnakeandLadder_UC2.SnakeLadderProg_Usecaase2();
+            
         }
     }
 }
---
+++ SnakeandLadder_UC1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadderProblem
+{
+    public class SnakeandLadder_UC1
+    {
+        public static void SnakeLadderProg_Usecase1()
+        {
+            
+        }
+    }
+}
