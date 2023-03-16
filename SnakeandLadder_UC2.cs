4dbdf39b5939aa562e5dd8f1076b4bdc694e1d87
“Niketanjenekar” <niketanjenekar31@gmail.com>
2023-03-16 23:35:36 +0530

    Snake and ladder game with use case 2

---
+++ SnakeandLadder_UC2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadderProblem
+{
+    internal class SnakeandLadder_UC2
+    {
+        public static void SnakeLadderProg_Usecaase2()
+        {
+            Console.WriteLine("The Single Player Snake and Ladder Game has started ");
+            Console.WriteLine("Player is at start Position 0");
+
+            Random random = new Random();
+
+            int diceroll = random.Next(1, 6);
+            Console.WriteLine("The dice rolls " +  diceroll + " and the Player is at Position " + diceroll);
+        }
+    }
+}
